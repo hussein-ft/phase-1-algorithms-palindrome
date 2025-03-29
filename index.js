2c64a1d116976d1b1a47445fb64204d6a62e6f99
Hussein <hussein.osman.@student.moringaschool.com>
2025-03-29 08:49:52 +0300

    seve complited chaanges

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+// Function to check if a string is a palindrome
+function isPalindrome(str) {
+  // Reverse the string
+  const reversedStr = str.split('').reverse().join('');
+  // Compare reversed string with the original string
+  return reversedStr === str;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+// Test cases
+console.log(isPalindrome("madam")); // Expected output: true
+console.log(isPalindrome("racecar")); // Expected output: true
+console.log(isPalindrome("robot")); // Expected output: false
+console.log(isPalindrome("level")); // Expected output: true
+console.log(isPalindrome("hello")); // Expected output: false
